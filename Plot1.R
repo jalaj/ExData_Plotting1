9b38d5b18bcd7dc6a82b26b71ae90663dbbf6da9
jalaj <jha.jalaj@gmail.com>
2014-10-13 05:35:38 +0530

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,10 @@
+## Read the data
+pc<-read.table("household_power_consumption.txt",header=T, sep=";")
+## Create a subset for the two dates
+df <- subset(pc, Date %in% c("1/2/2007", "2/2/2007"));
+## Remove the original object, too big!!
+rm(pc)
+## Crete the plot
+histdata<-as.numeric(as.character(df$Global_active_power))
+dev.copy(png, file = "plot1.png")
+dev.off()
